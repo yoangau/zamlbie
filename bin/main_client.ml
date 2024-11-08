d6baf95f88aa3991aa50e17c85a13d266810d9c3
DIGarnier <davidigarnier@gmail.com>
2024-11-07 23:23:11 -0500

    moving around stuff

--- bin/main_client.ml
+++ bin/main_client.ml
@@ -1,3 +1,12 @@
 open Zamlbie
 
-let () = Client.run () |> ignore
+let () =
+  let run =
+    let open Lwt.Infix in
+    Rest_client.create_game Game.default_config
+    >>= function
+    | None -> failwith "Game creation failed!"
+    | Some game -> Client.join_game game.game_id
+  in
+  Lwt_main.run run
+;;

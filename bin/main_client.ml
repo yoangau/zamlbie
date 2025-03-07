866021ac6a9d2233c70936c64ace0e522f920f38
Yoan Gauthier <yoangau@gmail.com>
2025-03-06 19:18:39 -0800

    Add offline game mode

--- bin/main_client.ml
+++ bin/main_client.ml
@@ -5,6 +5,7 @@ let () =
     let open Lwt.Infix in
     match Client_arg.parse_args () with
     | Ok (Client_arg.Join id) -> Client.join_game (Notty_lwt.Term.create ()) id
+    | Ok (Client_arg.Test config) -> Client.offline_game (Notty_lwt.Term.create ()) config
     | Ok (Client_arg.Create config) ->
       Client.create_game config
       >>= (function
